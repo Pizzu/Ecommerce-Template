9ce965084f5c979d5328b1371fa34803de0cee90
Pizzu <lucaloforte3@gmail.com>
2022-03-21 19:42:05 +0100

    Add ownedCoursesQuery and display owned courses in dashboard page

--- lib/sanity/query/index.ts
+++ lib/sanity/query/index.ts
@@ -7,6 +7,15 @@ export const coursesQuery = `*[_type == "course"] {
   coverImage
 }`
 
+export const ownedCoursesQuery = `*[_type == "course" && _id in $coursesIDs] {
+  _id,
+  title,
+  type,
+  description,
+  slug,
+  coverImage
+}`
+
 export const courseQuery = `*[_type == "course" && slug.current == $slug][0] {
   _id,
   title,
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,16 @@
+import { OwnedCourse, User } from "@prisma/client";
+import { prisma } from "@lib/prisma";
 import { GetServerSideProps, NextPage } from "next";
 import { getSession } from "next-auth/react";
+import { sanityClient } from "@lib/sanity";
+import { ownedCoursesQuery } from "@lib/sanity/query";
+import type { Course } from "../types";
 
-const DashboardPage: NextPage = () => {
+const DashboardPage: NextPage<{ courses: [Course] }> = ({courses}) => {
   return (
     <div className="container">
       <h1>Dashboard page</h1>
+      <h3>{courses[0].title}</h3>
     </div>
   )
 }
@@ -24,7 +30,16 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
 
+  // We retrive the current user from the session and we fetch it from the db with all the ownedCourses
+  const currentUser = await prisma.user.findUnique({where: {email: session.user?.email as string}, include: {ownedCourses: true} })
+  const coursesIDs = currentUser?.ownedCourses.map(ownedCourse => ownedCourse.key)
+  // We query exactly the courses owned by that user from sanity
+  const courses: [Course] = await sanityClient.fetch(ownedCoursesQuery, {coursesIDs})
+
   return {
-    props: { session },
+    props: { 
+      session,
+      courses
+    },
   }
 }
