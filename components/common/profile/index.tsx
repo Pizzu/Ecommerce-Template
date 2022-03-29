cd41d75268a4c8a0c595343d6834abfd32449d4e
Pizzu <lucaloforte3@gmail.com>
2022-03-29 13:03:36 -0400

    Create Profile Page and ProfileItem Components

---
+++ components/common/profile/index.tsx
@@ -0,0 +1,30 @@
+// Libraries
+// Types
+// Components
+import Image from "next/image";
+import { ButtonLink } from "@components/common";
+
+type ProfileItemProps = {
+  iconURL: string,
+  caption: string,
+  ctaText: string,
+  ctaURL: string
+}
+
+const ProfileItem: React.FC<ProfileItemProps> = ({iconURL, caption, ctaText, ctaURL}) => {
+  return (
+    <div className="bg-white p-8 shadow-xl">
+      <div className="text-center">
+        <div className="relative w-[3.63rem] h-[3.63rem] m-auto mb-6">
+          <Image src={iconURL} alt="star" layout="fill" />
+        </div>
+        <p className="caption mb-16">{caption}</p>
+        <div>
+          <ButtonLink href={ctaURL} className="bg-primary text-white">{ctaText}</ButtonLink>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileItem
---
+++ pages/profile/index.tsx
@@ -0,0 +1,53 @@
+// Libraries
+import { useAccount } from "@providers/AccountProvider";
+import { getSession } from "next-auth/react";
+// Types
+import { GetServerSideProps, NextPage } from "next";
+// Components
+import { ProfileItem } from "@components/common";
+
+const ProfilePage: NextPage = () => {
+
+  const { user } = useAccount()
+
+  return (
+    <main>
+      <section>
+        <div className="container py-[9rem]">
+          <div className="mb-8">
+            <h1>Your Profile</h1>
+            <p>Hi {user?.name}. Your current membership status is
+              <span className="ml-2 text-secondary">{user?.proPlan ? "PRO" : "BASIC"}</span>.
+            </p>
+          </div>
+          <div className="grid grid-cols-3 gap-6">
+            <ProfileItem iconURL="/receipt.svg" caption="Check your orders and download receipts here" ctaText="My Orders" ctaURL="/profile/orders" />
+            <ProfileItem iconURL="/contract.svg" caption="Check and manage your current subscription here" ctaText="My Subscription" ctaURL="/profile/orders" />
+            <ProfileItem iconURL="/rocket.svg" caption="Upgrade to PRO and enjoy all the courses" ctaText="Become PRO Now" ctaURL="/profile/orders" />
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default ProfilePage
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      session,
+    },
+  }
+}
