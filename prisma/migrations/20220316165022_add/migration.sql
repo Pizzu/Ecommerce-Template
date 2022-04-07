4268ca5a9f5ab7ee0cfe1817476cea4c0a4cc2e9
Pizzu <lucaloforte3@gmail.com>
2022-04-07 17:20:57 -0400

    Change DB from sqlite to postgresql and add local connection string

--- prisma/migrations/20220316165022_add/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "OwnedCourse" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "key" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "OwnedCourse_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
